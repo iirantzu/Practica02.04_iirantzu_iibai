3a2c358aa9171736ac6425a57b983be2ff80172d
iirantzu <113340976+iirantzu@users.noreply.github.com>
2022-10-25 20:39:14 +0200

    Practica02.04

--- Ejercicio 9.py
+++ Ejercicio 9.py
@@ -5,9 +5,9 @@
 #7 5 3 1
 #9 7 5 3 1
 
-numero_filas = int(input("Introduce el número de lineas de la piramide:\n"))
+num_filas = int(input("Introduce el número de lineas de la piramide:\n"))
 
-for fila in range(1, numero_filas+1):
-    for i in range(fila):
-        print("*", end=" ")
+for fila in range(1, num_filas + 1, 2):
+    for j in range(fila, 0, -2):
+        print(j, end=" ")
     print("")
